2d54ef3af0ab25e236388df53297c51c2f14dfa4
Themicrobit <102502255+Themicrobit@users.noreply.github.com>
2022-03-29 13:16:19 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showString("Hello!")
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . # .
+            # . # . #
+            `)
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
